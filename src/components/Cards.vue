84f36d0dcd15baca47ef91919591f52d6740379f
Alfredodaniello <alfredo_daniello@libero.it>
2024-04-15 14:41:19 +0200

    add static card component

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -5,9 +5,25 @@ export default {
 </script>
 
 <template>
-ciaoo
+<div class="card text-center">
+    <div class="card-image">
+        <img src="https://images.ygoprodeck.com/images/cards/34541863.jpg" alt="">
+    </div>
+    <h5>ciao</h5>
+    <span>ciao</span>
+</div>
 </template>
 
 <style scoped lang="scss">
-
+@use "../style/partials/variables" as *;
+.card {
+    color: white;
+    width: calc(100% / 6);
+    background-color: $brand-primary;
+    .card-image {
+        img{
+            width: 100%;
+        }
+    }
+}
 </style>
