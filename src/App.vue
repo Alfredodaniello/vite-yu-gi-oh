8976b495b1e06cbb59913a2009fb4e842c58fe1c
Alfredodaniello <alfredo_daniello@libero.it>
2024-04-15 14:52:01 +0200

    feat: add CardsContainer component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import Cards from './components/Cards.vue';
+import CardsContainer from './components/CardsContainer.vue';
 export default {
     components: {
         AppHeader,
-        Cards
+        Cards,
+        CardsContainer
     },
 }
 </script>
@@ -15,7 +17,7 @@ export default {
     <AppHeader></AppHeader>
 </header>
 <main>
-    <Cards></Cards>
+    <CardsContainer></CardsContainer>
 </main>
     
 
---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,29 @@
+<script>
+import Cards from './Cards.vue';
+export default {
+    components: {
+        Cards
+    },
+    name: "CardsContainer"
+}
+</script>
+
+<template>
+<div class="ms-container top"></div>
+<div class="ms-container">
+    <Cards></Cards>
+</div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+.top {
+    height: 70px;
+    background-color: black;
+}
+.ms-container{
+    width: 1200px;
+    margin: auto;
+}
+
+</style>
